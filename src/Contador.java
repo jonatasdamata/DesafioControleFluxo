b81d295d23ac9be9c76b0739a4b6570ae617ffb0
Jonatas da  Mata <jonatasdamata1@hotmail.com>
2024-07-22 16:25:17 -0300

    feat: adicionando códigos.

--- src/Contador.java
+++ src/Contador.java
@@ -1,2 +1,33 @@
-package PACKAGE_NAME;public class Contador {
+import java.util.Scanner;
+
+public class Contador {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Digite o primeiro parâmetro: ");
+        int parametroUm = scanner.nextInt();
+
+        System.out.println("Digite o segundo parâmetro: ");
+        int parametroDois = scanner.nextInt();
+
+        try {
+            //Lógica de contagem
+            contar(parametroUm, parametroDois);
+        } catch (ParametrosInvalidosException exception) {
+            System.out.println(exception.getMessage());
+        }
+    }
+
+    static void contar(int parametroUm, int parametroDois) throws ParametrosInvalidosException {
+
+        if (parametroUm < parametroDois) {
+            int contagem = parametroDois - parametroUm;
+            System.out.println("Imprimindo...");
+            for (int indice = 1; indice <= contagem; indice++) {
+                System.out.println("Imprimindo o número " + indice);
+            }
+        } else {
+            throw new ParametrosInvalidosException();
+        }
+    }
 }
